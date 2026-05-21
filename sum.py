1ab2836087c5902c6c41c6f11b1d3b823035543d
zannatul mawa <zannatulmawa38@gmail.com>
2026-05-21 16:26:50 +0600

    additon

--- sum.py
+++ sum.py
@@ -1,4 +1,6 @@
-a =0;
-b=0
-sum=a+b;
-print(sum)
+# Write a program to take a number and print whether it is positive, negative, or zero.
+a =int( input("enter a number"));
+if (a > 0) :
+    print("positive",a)
+else:
+    print("negative",a)
