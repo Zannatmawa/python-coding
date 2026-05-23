5efda18acba8759ba897cde378fdf4d4baa9bf8f
zannatul mawa <zannatulmawa38@gmail.com>
2026-05-23 15:54:36 +0600

    finding the multiple of 7

--- greatest_num.py
+++ greatest_num.py
@@ -4,11 +4,11 @@ a = int(input("Enter first number to check "))
 b = int(input("Enter second number to check "))
 c = int(input("Enter third number to check "))
 
-if(a>b):
-    if(a>c):
-       print("a is the greatest")
-    else:
-       print("c is the greatest")
+if(a>=b and a>=c):
+       print("a is the greatest",a)
+
+elif(b>=c):
+ print("b is the greatest",b)
 else:
- print("b is the greatest")
+       print("c is the greatest",c)
 
---
+++ is_multiple.py
@@ -0,0 +1,7 @@
+#write a prgogram to check if a number is a multiple of 7 or not.
+
+a = int(input("Enter first number to check "))
+if(a%7==0):
+    print("multiple of 7")
+else:
+    print("not a multiple")
