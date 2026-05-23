0501227dd9399f0c2cb162360e1dfcec5e7eaa16
zannatul mawa <zannatulmawa38@gmail.com>
2026-05-23 16:11:04 +0600

    making list of favourite 3 movies

---
+++ list_and_tuple/store_movies.py
@@ -0,0 +1,13 @@
+#write a program to ask the user names of their 3 favourite movies and store them in a list
+
+movies1 = str(input("Enter your 1st favourite movies: "))
+movies2 = str(input("Enter your 2nd favourite movies: "))
+movies3 = str(input("Enter your 3rd favourite movies: "))
+
+li=[]
+
+li.append(movies1)
+li.append(movies2)
+li.append(movies3)
+
+print(li)
