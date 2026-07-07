88bed1bf5c9d5e23e97238b13cdbb6c460e0ecb0
zannatul mawa <zannatulmawa38@gmail.com>
2026-07-07 23:10:42 +0600

    sum of digit  problem

--- list_and_tuple/palindorm.py
+++ list_and_tuple/palindorm.py
@@ -1,10 +1,9 @@
 #write a program to check if a list contain a palindrome of elements
 # name=str(input("Enter a string:"))
 
-li=[1,2,4]
-copy_li = li.copy()
-copy_li.reverse()
-if(copy_li==li):
-    print("palindorm")
+text = input("Enter a string: ")
+
+if text == text[::-1]:
+    print("Palindrome")
 else:
-    print("not")
+    print("Not Palindrome")
